a14eaf87e8f66cbf0743311e79c02d8f793a6f1b
Jay Li <j_li@g.harvard.edu>
2019-08-12 13:11:16 -0700

    comment main.cpp

--- src/examples/main.cpp
+++ src/examples/main.cpp
@@ -6,20 +6,30 @@ extern Application *getApplication();
 
 Application *app;
 
+/// \brief Create GLUT window
 void createWindow(const char *title)
 {
+  // Init glut with double buffering, RGBA, and depth
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  
+  // Set window size in pixels
   glutInitWindowSize(640, 320);
+
+  // Set window position from upper-left corner
   glutInitWindowPosition(0, 0);
+
+  // Create window with title
   glutCreateWindow(title);
 }
 
+/// \brief Process background update when idle
 void update()
 {
   TimingData::get().update();
   app->update();
 }
 
+/// \brief Display next frame
 void display()
 {
   app->display();
@@ -27,21 +37,25 @@ void display()
   glutSwapBuffers();
 }
 
+/// \brief Process mouse events
 void mouse(int button, int state, int x, int y)
 {
   app->mouse(button, state, x, y);
 }
 
+/// \brief Resize window
 void reshape(int width, int height)
 {
   app->resize(width, height);
 }
 
+/// \brief Process keyboard input
 void keyboard(unsigned char key, int /* x */, int /* y */)
 {
   app->key(key);
 }
 
+/// \brief Process mouse drag event
 void motion(int x, int y)
 {
   app->mouseDrag(x, y);
@@ -49,12 +63,15 @@ void motion(int x, int y)
 
 int main(int argc, char** argv)
 {
+  // Init application
   glutInit(&argc, argv);
   TimingData::init();
 
+  // Create GLUT window
   app = getApplication();
   createWindow(app->getTitle());
 
+  // Register GLUT callbacks
   glutReshapeFunc(reshape);
   glutKeyboardFunc(keyboard);
   glutDisplayFunc(display);
@@ -62,9 +79,11 @@ int main(int argc, char** argv)
   glutMouseFunc(mouse);
   glutMotionFunc(motion);
 
+  // Start application
   app->initGraphics();
   glutMainLoop();
 
+  // Clean up resources
   app->deinit();
   delete app;
   TimingData::deinit();
