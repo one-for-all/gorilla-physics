413f0b497d7e24053737775e79e3dca7fb1855e3
one-for-all <j_li@g.harvard.edu>
2019-08-12 00:07:07 -0700

    Change original random generator implementation to use C++11 <random> library `

--- include/gorilla/random.h
+++ include/gorilla/random.h
@@ -1,29 +1,40 @@
 #ifndef GORILLA_RANDOM_H
 #define GORILLA_RANDOM_H
 
+#include <random>
+
 #include "core.h"
 
 namespace gorilla {
   class Random
   {
   public:
-    unsigned rotl(unsigned n, unsigned r);
-    unsigned rotr(unsigned n, unsigned r);
-
     Random();
-    Random(unsigned seed);
 
-    void seed(unsigned seed);
-
-    unsigned randomBits();
+    /// \brief Return random floating-point value in [0, 1)
     real randomReal();
+
+    /// \brief Return random floating-point value in [min, max)
     real randomReal(real min, real max);
+
+    /// \brief Return random integer in [0, max);
     unsigned randomInt(unsigned max);
+
+    /// \brief Return random vector in [min, max)
     Vector3 randomVector(const Vector3 &min, const Vector3 &max);
   
   private:
-    int p1, p2;
-    unsigned buffer[17];
+    /// \brief Device for obtaining seed
+    std::random_device rd;
+
+    /// \brief Standard mersenne_twister_engine
+    std::mt19937 gen;
+
+    /// \brief Uniform floating-point distribution
+    std::uniform_real_distribution<real> real_dist;
+
+    /// \brief Uniform integer distribution
+    std::uniform_int_distribution<int> int_dist;
   };
 }
 
--- src/random.cpp
+++ src/random.cpp
@@ -4,66 +4,13 @@
 
 using namespace gorilla;
 
-Random::Random()
+Random::Random() : rd(), gen(rd()), real_dist(0, 1), int_dist(0)
 {
-  seed(0);
-}
-
-Random::Random(unsigned seed)
-{
-  Random::seed(seed);
-}
-
-void Random::seed(unsigned s)
-{
-  if (s == 0)
-    s = (unsigned)clock();
-  
-  for (unsigned i = 0; i < 17; ++i)
-  {
-    s = s * 2891336453 + 1;
-    buffer[i] = s;
-  }
-
-  p1 = 0;
-  p2 = 0;
-}
-
-unsigned Random::rotl(unsigned n, unsigned r)
-{
-  return (n << r) | (n >> (32 -r));
-}
-
-unsigned Random::rotr(unsigned n, unsigned r)
-{
-  return (n >> r) | (n << (32 - r));
-}
-
-unsigned Random::randomBits()
-{
-  unsigned result;
-  result = buffer[p1] = rotl(buffer[p2], 13) + rotl(buffer[p1], 9);
-
-  if (--p1 < 0)
-    p1 = 16;
-  if (--p2 < 0)
-    p2 = 16;
-
-  return result;
 }
 
 real Random::randomReal()
 {
-  unsigned bits = randomBits();
-
-  union {
-    real value;
-    unsigned word;
-  } convert;
-
-  convert.word = (bits >> 9) | 0x3f800000;
-
-  return convert.value - 1.0f;
+  return real_dist(gen);
 }
 
 real Random::randomReal(real min, real max)
@@ -73,7 +20,7 @@ real Random::randomReal(real min, real max)
 
 unsigned Random::randomInt(unsigned max)
 {
-  return randomBits() % max;
+  return int_dist(gen) % max;
 }
 
 Vector3 Random::randomVector(const Vector3 &min, const Vector3 &max)
