11014c8e4a6c1ae89381c636d92b15db91cb9364
one-for-all <j_li@g.harvard.edu>
2019-08-23 22:47:56 +0800

    add particle world

--- include/gorilla/pworld.h
+++ include/gorilla/pworld.h
@@ -4,4 +4,64 @@
 #include "pfgen.h"
 #include "plinks.h"
 
+namespace gorilla {
+
+/// \brief An environment for simulating particles
+class ParticleWorld
+{
+public:
+  typedef std::vector<Particle*> Particles;
+  typedef std::vector<ParticleContactGenerator*> ContactGenerators;
+
+protected:
+  /// \brief Particles in the world
+  Particles particles;
+
+  /// \brief Force registry for particles
+  ParticleForceRegistry registry;
+
+  /// \brief Contact resolver for particles
+  ParticleContactResolver resolver;
+
+  /// \brief Particle contact generators
+  ContactGenerators contactGenerators;
+
+  /// \brief List of contacts
+  ParticleContact *contacts;
+
+  /// \brief Max num of contacts
+  std::size_t maxContacts;
+
+public:
+  /// \brief Constructor
+  ParticleWorld(std::size_t maxContacts);
+
+  /// \brief Destructor
+  ~ParticleWorld();
+
+  /// \brief Generate contacts in the world
+  /// \return Number of contacts generated
+  std::size_t generateContacts();
+
+  /// \brief Integrate the dynamics of all the particles for the given duration
+  void integrate(real duration);
+
+  /// \brief Process all physics in the particle world
+  void runPhysics(real duration);
+
+  /// \brief Initialize the world for the next frame
+  void startFrame();
+
+  /// \brief Get all the particles
+  Particles& getParticles();
+
+  /// \brief Get all contact generators
+  ContactGenerators& getContactGenerators();
+
+  /// \brief Get force registry
+  ParticleForceRegistry& getForceRegistry();
+
+};
+}
+
 #endif // GORILLA_PWORLD_H
---
+++ src/pworld.cpp
@@ -0,0 +1,87 @@
+#include <gorilla/pworld.h>
+
+using namespace gorilla;
+
+///////////////////////////////////////////////////////////////////
+ParticleWorld::ParticleWorld(std::size_t maxContacts)
+: maxContacts(maxContacts), resolver(2*maxContacts)
+{
+  this->contacts = new ParticleContact[maxContacts];
+}
+
+///////////////////////////////////////////////////////////////////
+ParticleWorld::~ParticleWorld()
+{
+  delete[] this->contacts;
+}
+
+///////////////////////////////////////////////////////////////////
+void ParticleWorld::startFrame()
+{
+  for(auto &particle : this->particles)
+  {
+    particle->clearAccumulator();
+  }
+}
+
+///////////////////////////////////////////////////////////////////
+std::size_t ParticleWorld::generateContacts()
+{
+  std::size_t limit = this->maxContacts;
+  ParticleContact *nextContact = this->contacts;
+
+  for (auto &contactGenerator : this->contactGenerators)
+  {
+    auto used = contactGenerator->addContact(nextContact, limit);
+    limit -= used;
+    nextContact += used;
+
+    if (limit <= 0)
+      break;
+  }
+
+  return this->maxContacts - limit;
+}
+
+///////////////////////////////////////////////////////////////////
+void ParticleWorld::integrate(real duration)
+{
+  for (auto &particle : this->particles)
+  {
+    particle->integrate(duration);
+  }
+}
+
+///////////////////////////////////////////////////////////////////
+void ParticleWorld::runPhysics(real duration)
+{
+  this->registry.updateForces(duration);
+
+  this->integrate(duration);
+
+  auto usedContacts = this->generateContacts();
+
+  if (usedContacts > 0)
+  {
+    this->resolver.setIterations(usedContacts * 2);
+    this->resolver.resolveContacts(this->contacts, usedContacts, duration);
+  }
+}
+
+///////////////////////////////////////////////////////////////////
+ParticleWorld::Particles& ParticleWorld::getParticles()
+{
+  return this->particles;
+}
+
+///////////////////////////////////////////////////////////////////
+ParticleWorld::ContactGenerators& ParticleWorld::getContactGenerators()
+{
+  return this->contactGenerators;
+}
+
+///////////////////////////////////////////////////////////////////
+ParticleForceRegistry& ParticleWorld::getForceRegistry()
+{
+  return this->registry;
+}
