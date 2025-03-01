bcaa8d022b3123162c4e7c91eaea0aebc36be684
Jay Li <jn.sapiens@gmail.com>
2025-03-01 22:11:17 +0800

    Example of ball hitting ground

---
+++ examples/ball.rs
@@ -0,0 +1,75 @@
+use gorilla_physics::contact::{ContactPoint, HalfSpace};
+use gorilla_physics::control::energy_control::Controller;
+use gorilla_physics::helpers::build_sphere_body;
+use gorilla_physics::interface::controller::NullController;
+use gorilla_physics::joint::floating::FloatingJoint;
+use gorilla_physics::mechanism::MechanismState;
+use gorilla_physics::plot::{plot, plot_trajectory};
+use gorilla_physics::simulate::step;
+use gorilla_physics::transform::Transform3D;
+use nalgebra::{dvector, vector, UnitQuaternion, Vector3};
+
+use gorilla_physics::joint::{Joint, JointPosition, JointVelocity};
+use gorilla_physics::{pose::Pose, spatial_vector::SpatialVector, types::Float};
+
+pub fn main() {
+    // Arrange
+    let m = 0.1;
+    let r = 1.0;
+    let v_x_init = 1.0;
+
+    let ball_frame = "ball";
+    let world_frame = "world";
+    let ball_to_world = Transform3D::identity(&ball_frame, &world_frame);
+
+    let ball = build_sphere_body(m, r, &ball_frame);
+
+    let treejoints = dvector![Joint::FloatingJoint(FloatingJoint::new(ball_to_world))];
+    let bodies = dvector![ball];
+    let mut state = MechanismState::new(treejoints, bodies);
+
+    let h_ground = -0.3;
+    let alpha = 2.0; // roughly how much velocity is lost
+    let mu = 1.0;
+    let ground = HalfSpace::new_with_params(Vector3::z_axis(), h_ground, alpha, mu);
+    state.add_halfspace(&ground);
+
+    state.add_contact_point(&ContactPoint {
+        frame: ball_frame.to_string(),
+        location: vector![0.0, 0.0, 0.0],
+    });
+
+    let q_init = vec![JointPosition::Pose(Pose {
+        rotation: UnitQuaternion::identity(),
+        translation: vector![0.0, 0.0, 0.0],
+    })];
+    let v_init = vec![JointVelocity::Spatial(SpatialVector {
+        angular: vector![0.0, 0.0, 0.0],
+        linear: vector![v_x_init, 0.0, 0.0],
+    })];
+    state.update(&q_init, &v_init);
+
+    // Act
+    let final_time = 5.0;
+    let dt = 1e-3;
+
+    let num_steps = (final_time / dt) as usize;
+
+    let mut controller = NullController {};
+
+    let mut data1: Vec<Float> = Vec::with_capacity(num_steps);
+    let mut data2: Vec<Float> = Vec::with_capacity(num_steps);
+
+    for s in 0..num_steps {
+        let torque = controller.control(&mut state);
+        let (q, v) = step(&mut state, dt, &torque);
+
+        let pose_body = &state.poses()[0];
+
+        data1.push(pose_body.translation.x);
+        data2.push(pose_body.translation.z);
+    }
+
+    plot(&data1, final_time, dt, num_steps, "plot");
+    plot_trajectory(&data1, &data2, "traj");
+}
