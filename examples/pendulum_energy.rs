e8d21cbc475799b819681ba0e2c8e14fea9ce301
Jay Li <jn.sapiens@gmail.com>
2025-03-05 17:22:13 +0800

    Pendulum energy example

--- examples/pendulum_energy.rs
+++ examples/pendulum_energy.rs
@@ -1,11 +1,12 @@
+use gorilla_physics::joint::ToJointTorqueVec;
+use gorilla_physics::plot::plot;
 use gorilla_physics::{
     helpers::build_pendulum, mechanism::MechanismState, simulate::step, types::Float, GRAVITY,
 };
 use nalgebra::{dvector, vector, DVector, Matrix3, Matrix4};
-use plotters::prelude::*;
 
 fn potential_energy(state: &MechanismState, l: &Float) -> Float {
-    let q = state.q[0];
+    let q = state.q[0].float();
     let m = state.bodies.get(0).unwrap().inertia.mass;
 
     let h = 0.5 * l * -q.sin();
@@ -37,40 +38,13 @@ pub fn main() {
     let dt = 0.01;
     let num_steps = (final_time / dt) as usize;
     while t < final_time {
-        let _ = step(&mut state, dt, &dvector![0.0]);
+        let _ = step(&mut state, dt, &vec![0.0].to_joint_torque_vec());
         KEs.extend([state.kinetic_energy()]);
         PEs.extend([potential_energy(&state, &l)]);
         t += dt;
     }
 
-    let data = KEs + PEs;
+    let data = (KEs + PEs).as_slice().to_vec();
 
-    // Determine y-axis limits based on the minimum and maximum values in the data
-    let min_y = data.iter().cloned().fold(Float::INFINITY, Float::min);
-    let max_y = data.iter().cloned().fold(Float::NEG_INFINITY, Float::max);
-
-    // Create a plotting area
-    let root = BitMapBackend::new("plot.png", (640, 480)).into_drawing_area();
-    let _ = root.fill(&WHITE);
-
-    // Configure the chart
-    let mut chart = ChartBuilder::on(&root)
-        .caption("Kinetic Energy vs. Time plot", ("sans-serif", 20))
-        .x_label_area_size(30)
-        .y_label_area_size(40)
-        .build_cartesian_2d(0.0..final_time, min_y..max_y)
-        .unwrap();
-
-    // Customize the chart
-    let _ = chart.configure_mesh().draw();
-
-    // Plot the data
-    let _ = chart.draw_series(LineSeries::new(
-        (0..num_steps).map(|i| (i as Float * dt, data[i])),
-        &BLUE,
-    ));
-
-    // Present the result
-    root.present()
-        .expect("Unable to present the result to the screen");
+    plot(&data, final_time, dt, num_steps, "pendulum energy");
 }
