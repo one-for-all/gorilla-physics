ecf91a39cf42cd35662a0f2b265a90f1dc32f1c6
Jay Li <jn.sapiens@gmail.com>
2025-04-19 20:21:07 +0900

    Add two examples

---
+++ examples/LSLIP.rs
@@ -0,0 +1,146 @@
+use gorilla_physics::spatial::transform::Matrix4Ext;
+use gorilla_physics::{
+    contact::{ContactPoint, HalfSpace},
+    control::Controller,
+    inertia::SpatialInertia,
+    interface::controller::NullController,
+    joint::{
+        floating::FloatingJoint,
+        prismatic::{JointSpring, PrismaticJoint},
+        Joint, JointPosition, JointVelocity,
+    },
+    mechanism::MechanismState,
+    plot::{plot, plot_trajectory},
+    rigid_body::RigidBody,
+    simulate::step,
+    spatial::pose::Pose,
+    spatial::spatial_vector::SpatialVector,
+    spatial::transform::Transform3D,
+    types::Float,
+    WORLD_FRAME,
+};
+use nalgebra::{vector, Matrix3, Matrix4, UnitQuaternion, Vector3};
+
+/// Lossy Spring Loaded Inverted Pendulum (LSLIP)
+/// Ref: Nonlinear Model Predictive Control for Rough-Terrain Robot Hopping, 2012
+/// ALSLIP without the prismatic actuation
+pub fn main() {
+    let m_body = 0.54;
+    let r_body = 1.0;
+    let m_leg = 0.1;
+    let r_leg = 1.0;
+    let l_leg = 0.2;
+
+    let l_rest_spring = 0.0;
+    let k_spring = 500.0;
+
+    // Create hopper body
+    let moment_x = 2.0 / 5.0 * m_body * r_body * r_body;
+    let moment_body = Matrix3::from_diagonal(&vector![moment_x, moment_x, moment_x]);
+    let cross_part_body = vector![0.0, 0.0, 0.0];
+
+    let body_frame = "body";
+    let body_to_world = Transform3D::identity(&body_frame, WORLD_FRAME);
+    let body = RigidBody::new(SpatialInertia {
+        frame: body_frame.to_string(),
+        moment: moment_body,
+        cross_part: cross_part_body,
+        mass: m_body,
+    });
+
+    // Create hopper leg
+    let moment_x = 2.0 / 5.0 * m_leg * r_leg * r_leg;
+    let moment_leg = Matrix3::from_diagonal(&vector![moment_x, moment_x, moment_x]);
+    let cross_part_leg = vector![0.0, 0.0, 0.0];
+    let axis_leg = vector![0.0, 0.0, -1.0];
+
+    let leg_frame = "leg";
+    let leg_to_body = Transform3D {
+        from: leg_frame.to_string(),
+        to: body_frame.to_string(),
+        mat: Matrix4::<Float>::move_z(-l_leg),
+    };
+    let leg = RigidBody::new(SpatialInertia {
+        frame: leg_frame.to_string(),
+        moment: moment_leg,
+        cross_part: cross_part_leg,
+        mass: m_leg,
+    });
+
+    // Create the hopper
+    let spring = JointSpring {
+        k: k_spring,
+        l: l_rest_spring,
+    };
+    let treejoints = vec![
+        Joint::FloatingJoint(FloatingJoint {
+            init_mat: body_to_world.mat.clone(),
+            transform: body_to_world,
+        }),
+        Joint::PrismaticJoint(PrismaticJoint::new_with_spring(
+            leg_to_body,
+            axis_leg,
+            spring,
+        )),
+    ];
+
+    let bodies = vec![body, leg];
+    let mut state = MechanismState::new(treejoints, bodies);
+
+    let h_ground = -0.3;
+    let alpha = 1.0; // velocity lost
+    let mu = 0.5;
+    let ground = HalfSpace::new_with_params(Vector3::z_axis(), h_ground, alpha, mu);
+    state.add_halfspace(&ground);
+
+    state.add_contact_point(&ContactPoint::new(leg_frame, vector![0., 0., 0.]));
+
+    let q_init = vec![
+        JointPosition::Pose(Pose {
+            rotation: UnitQuaternion::identity(),
+            translation: vector![0.0, 0.0, 0.0],
+        }),
+        JointPosition::Float(0.0),
+    ];
+    let v_init = vec![
+        JointVelocity::Spatial(SpatialVector {
+            angular: vector![0.0, 0.0, 0.0],
+            linear: vector![0.1, 0.0, 0.0],
+        }),
+        JointVelocity::Float(0.0),
+    ];
+    state.update(&q_init, &v_init);
+
+    // Simulate
+    let final_time = 1.5;
+    let dt = 1.0 / 2000.0;
+    let num_steps = (final_time / dt) as usize;
+
+    // let mut controller = ALSLIPController::new(k_spring, l_rest_spring);
+    let mut controller = NullController {};
+
+    let mut data1: Vec<Float> = Vec::with_capacity(num_steps);
+    let mut data2: Vec<Float> = Vec::with_capacity(num_steps);
+    let mut data3: Vec<Float> = Vec::with_capacity(num_steps);
+    let mut v_z_prev = 0.0;
+
+    for s in 0..num_steps {
+        let torque = controller.control(&mut state, None);
+        let (q, v) = step(
+            &mut state,
+            dt,
+            &torque,
+            &gorilla_physics::integrators::Integrator::SemiImplicitEuler,
+        );
+
+        let pose_body = &state.poses()[0]; // q[0].pose();
+        let pose_leg = &state.poses()[1];
+
+        data1.push(pose_leg.translation.x);
+        data2.push(pose_leg.translation.z);
+        data3.push(pose_leg.translation.x);
+    }
+
+    plot(&data3, final_time, dt, num_steps, "plot");
+    plot_trajectory(&data1, &data2, "traj");
+}
---
+++ examples/spring_drop.rs
@@ -0,0 +1,94 @@
+use gorilla_physics::contact::{ContactPoint, HalfSpace};
+use gorilla_physics::joint::floating::FloatingJoint;
+use gorilla_physics::joint::prismatic::{JointSpring, PrismaticJoint};
+use gorilla_physics::joint::{Joint, JointVelocity};
+use gorilla_physics::mechanism::MechanismState;
+use gorilla_physics::plot::plot;
+use gorilla_physics::rigid_body::RigidBody;
+use gorilla_physics::simulate::step;
+use gorilla_physics::spatial::transform::Matrix4Ext;
+use gorilla_physics::spatial::transform::Transform3D;
+use gorilla_physics::WORLD_FRAME;
+use gorilla_physics::{spatial::spatial_vector::SpatialVector, types::Float};
+use nalgebra::{vector, zero, Matrix4, Vector3};
+
+/// Simulate a spring connected by two spheres dropping to ground
+pub fn main() {
+    let m_body = 1.0;
+    let r_body = 1.0;
+    let m_foot = 1.0;
+    let r_foot = 1.0;
+    let l_leg = 1.0;
+    let l_spring_rest = 0.0;
+    let k_spring = 100.0;
+
+    let body_frame = "body";
+    let body = RigidBody::new_sphere(m_body, r_body, &body_frame);
+    let body_to_world = Transform3D::identity(body_frame, WORLD_FRAME);
+
+    let foot_frame = "foot";
+    let foot = RigidBody::new_sphere(m_foot, r_foot, &foot_frame);
+    let foot_to_body = Transform3D {
+        from: foot_frame.to_string(),
+        to: body_frame.to_string(),
+        mat: Matrix4::<Float>::move_z(-l_leg),
+    };
+    let axis_leg = vector![0.0, 0.0, -1.0];
+
+    let spring = JointSpring {
+        k: k_spring,
+        l: l_spring_rest,
+    };
+    let treejoints = vec![
+        Joint::FloatingJoint(FloatingJoint::new(body_to_world)),
+        Joint::PrismaticJoint(PrismaticJoint::new_with_spring(
+            foot_to_body,
+            axis_leg,
+            spring,
+        )),
+    ];
+    let bodies = vec![body, foot];
+    let mut state = MechanismState::new(treejoints, bodies);
+    state.add_contact_point(&ContactPoint::new(foot_frame, Vector3::zeros()));
+
+    let h_ground = -2.0;
+    let alpha = 1.0;
+    let mu = 0.0;
+    let ground = HalfSpace::new_with_params(Vector3::z_axis(), h_ground, alpha, mu);
+    state.add_halfspace(&ground);
+
+    state.set_joint_v(
+        1,
+        JointVelocity::Spatial(SpatialVector {
+            angular: zero(),
+            linear: vector![0.1, 0.0, 0.0],
+        }),
+    );
+
+    // Act
+    let final_time = 2.5;
+    let dt = 1e-3;
+    let num_steps = (final_time / dt) as usize;
+    let mut data1: Vec<Float> = Vec::with_capacity(num_steps);
+    let mut prev_v_spring = 0.0;
+    for s in 0..num_steps {
+        let torque = vec![];
+        let (q, v) = step(
+            &mut state,
+            dt,
+            &torque,
+            &gorilla_physics::integrators::Integrator::SemiImplicitEuler,
+        );
+
+        let v_spring = v[1].float();
+        if prev_v_spring < 0.0 && *v_spring >= 0.0 {
+            println!("bottom");
+        }
+        prev_v_spring = *v_spring;
+
+        let pose_foot = &state.poses()[1];
+        data1.push(pose_foot.translation.z);
+    }
+
+    plot(&data1, final_time, dt, num_steps, "spring drop");
+}
