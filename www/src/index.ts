225a5f3bb3f6310559f64767ff3478ace9761ed2
Jay Li <jn.sapiens@gmail.com>
2025-04-19 21:47:31 +0900

    Add wasd keys visual

--- www/src/index.ts
+++ www/src/index.ts
@@ -33,7 +33,7 @@ import("gorilla-physics").then((gorilla) => {
 
   // Important: Set initial camera position
   let cameraPosition = {
-    eye: { x: 0.0, y: -10.0, z: 1.0 },
+    eye: { x: 0.0, y: -10.0, z: 5.0 },
     target: { x: 0.0, y: 0, z: 0.0 },
   };
   simulator.graphics.lookAt(cameraPosition);
