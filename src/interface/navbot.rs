9eee819347e34479e932b5c1b9f368bd1a4fa217
Jay Li <jn.sapiens@gmail.com>
2025-07-19 23:40:41 +0900

    performance: parallelize file read, and time it
    * parallelizing file read alone doesn't seem to speed it up

--- src/interface/navbot.rs
+++ src/interface/navbot.rs
@@ -1,9 +1,12 @@
+use futures::future::join_all;
 use na::{vector, UnitQuaternion};
-use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::prelude::*;
+use web_sys::window;
 
 use crate::{
     builders::navbot::{build_balancing_bot, build_navbot, build_navbot_motor, NavbotMeshes},
     collision::mesh::Mesh,
+    flog,
     joint::JointPosition,
     spatial::pose::Pose,
 };
@@ -28,52 +31,47 @@ pub async fn createNavbotMotor() -> InterfaceMechanismState {
 
 #[wasm_bindgen]
 pub async fn createNavbot() -> InterfaceMechanismState {
+    let performance = window().unwrap().performance().unwrap();
+    let start = performance.now();
+
     let mut navbot_meshes = NavbotMeshes::new();
-    let buf = read_web_file("navbot/esp32pcb.obj").await;
-    let mesh = Mesh::new_from_obj(&buf);
-    navbot_meshes.esp32pcb = Some(mesh);
 
-    let buf = read_web_file("navbot/top_plate.obj").await;
-    let mesh = Mesh::new_from_obj(&buf);
-    navbot_meshes.top_plate = Some(mesh);
+    let file_paths = vec![
+        "navbot/esp32pcb.obj",
+        "navbot/top_plate.obj",
+        "navbot/side_plate_left.obj",
+        "navbot/side_plate_right.obj",
+        "navbot/leg_inner_left.obj",
+        "navbot/leg_outer_left.obj",
+        "navbot/pin.obj",
+        "navbot/foot_motor_left.obj",
+        "navbot/encoder.obj",
+        "navbot/foot_plate_left.obj",
+        "navbot/link_plate_left.obj",
+    ];
 
-    let buf = read_web_file("navbot/side_plate_left.obj").await;
-    let mesh = Mesh::new_from_obj(&buf);
-    navbot_meshes.side_plate_left = Some(mesh);
+    let fetches = file_paths.iter().map(|path| read_web_file(path));
+    let buffers: Vec<String> = join_all(fetches).await;
+    for (i, buf) in buffers.iter().enumerate() {
+        let mesh = Some(Mesh::new_from_obj(buf));
+        match i {
+            0 => navbot_meshes.esp32pcb = mesh,
+            1 => navbot_meshes.top_plate = mesh,
+            2 => navbot_meshes.side_plate_left = mesh,
+            3 => navbot_meshes.side_plate_right = mesh,
+            4 => navbot_meshes.leg_inner_left = mesh,
+            5 => navbot_meshes.leg_outer_left = mesh,
+            6 => navbot_meshes.foot_pin_left = mesh,
+            7 => navbot_meshes.foot_motor_left = mesh,
+            8 => navbot_meshes.foot_encoder_left = mesh,
+            9 => navbot_meshes.foot_plate_left = mesh,
+            10 => navbot_meshes.link_plate_left = mesh,
+            _ => {}
+        }
+    }
 
-    let buf = read_web_file("navbot/side_plate_right.obj").await;
-    let mesh = Mesh::new_from_obj(&buf);
-    navbot_meshes.side_plate_right = Some(mesh);
-
-    // Leg left
-    let buf = read_web_file("navbot/leg_inner_left.obj").await;
-    let mesh = Mesh::new_from_obj(&buf);
-    navbot_meshes.leg_inner_left = Some(mesh);
-
-    let buf = read_web_file("navbot/leg_outer_left.obj").await;
-    let mesh = Mesh::new_from_obj(&buf);
-    navbot_meshes.leg_outer_left = Some(mesh);
-
-    // Foot left
-    let pin_buf = read_web_file("navbot/pin.obj").await;
-    let pin_mesh = Mesh::new_from_obj(&pin_buf);
-    navbot_meshes.foot_pin_left = Some(pin_mesh);
-
-    let buf = read_web_file("navbot/foot_motor_left.obj").await;
-    let mesh = Mesh::new_from_obj(&buf);
-    navbot_meshes.foot_motor_left = Some(mesh);
-
-    let encoder_buf = read_web_file("navbot/encoder.obj").await;
-    let encoder_mesh = Mesh::new_from_obj(&encoder_buf);
-    navbot_meshes.foot_encoder_left = Some(encoder_mesh);
-
-    let buf = read_web_file("navbot/foot_plate_left.obj").await;
-    let mesh = Mesh::new_from_obj(&buf);
-    navbot_meshes.foot_plate_left = Some(mesh);
-
-    let buf = read_web_file("navbot/link_plate_left.obj").await;
-    let mesh = Mesh::new_from_obj(&buf);
-    navbot_meshes.link_plate_left = Some(mesh);
+    let end = performance.now();
+    flog!("loading mesh took {:.3} s", (end - start) / 1000.); // currently takes ~2 seconds
 
     let state = build_navbot(navbot_meshes);
 
--- src/interface/util.rs
+++ src/interface/util.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{window, Response};
+use web_sys::{js_sys, window, Response, TextDecoder};
 
 /// Read a file into a string. File needs to be hosted on the site.
 pub async fn read_web_file(file_path: &str) -> String {
@@ -11,6 +11,17 @@ pub async fn read_web_file(file_path: &str) -> String {
     let resp: Response = resp_value.dyn_into().unwrap();
     let buf = JsFuture::from(resp.text().unwrap()).await.unwrap();
     let buf = buf.as_string().unwrap();
+
+    // // Supposed to be faster to do the decoding in Rust
+    // let buf = JsFuture::from(resp.array_buffer().unwrap()).await.unwrap();
+    // // Convert ArrayBuffer -> Uint8Array
+    // let uint8_array = js_sys::Uint8Array::new(&buf);
+
+    // // Decode using TextDecoder
+    // let decoder = TextDecoder::new().expect("failed to create TextDecoder");
+    // let buf = decoder
+    //     .decode_with_js_u8_array(&uint8_array)
+    //     .expect("text decode failed");
 
     buf
 }
