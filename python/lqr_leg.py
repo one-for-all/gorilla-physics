506c24abcd40e7a6943b7a2937b2c09cc542506b
Jay Li <jn.sapiens@gmail.com>
2025-08-22 18:22:07 +0900

    feature: add python file for computing leg lqr solution

---
+++ python/lqr_leg.py
@@ -0,0 +1,23 @@
+import control
+import numpy as np
+
+A = np.matrix([[0., 1. ], [0., 0.]])
+B = np.matrix([[0.], [1.]])
+
+C = np.matrix([[1., 0.]])
+
+z_com = 0.09428090415820635
+D = -z_com/9.81 * np.identity(1)
+
+Q = C.T * C
+R = D.T * D
+N = C.T * D
+
+K, S, E = control.lqr(A, B, Q, R, N)
+
+print("LQR Gain K:")
+print(K)
+print("\nSolution to Riccati Equation S:")
+print(S)
+print("\nClosed-loop eigenvalues E:")
+print(E)
