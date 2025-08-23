aed25181d0496937784387a3c50752e19ac41beb
Jay Li <jn.sapiens@gmail.com>
2025-08-23 19:15:14 +0900

    feature: add control of base floating joint

--- src/control/leg_control.rs
+++ src/control/leg_control.rs
@@ -1,12 +1,17 @@
 use clarabel::{
-    algebra::{CscMatrix, MatrixMathMut},
-    solver::{DefaultSettings, DefaultSolver, IPSolver},
+    algebra::CscMatrix,
+    solver::{DefaultSettings, DefaultSolver, IPSolver, SupportedConeT::ZeroConeT},
 };
-use na::{vector, DMatrix, Matrix3, Matrix3x6, Vector2, Vector3};
+use na::Vector3;
+use na::{vector, DMatrix, DVector, Matrix1xX, Matrix6xX, Vector6};
 
 use crate::{
-    control::Controller, flog, joint::JointTorque, spatial::spatial_vector::SpatialVector,
-    types::Float, util::skew_symmetric, GRAVITY, PI,
+    control::Controller,
+    flog,
+    joint::{JointTorque, ToFloatDVec},
+    spatial::spatial_vector::SpatialVector,
+    types::Float,
+    GRAVITY, PI,
 };
 
 /// QP based controller with ZMP dynamics
@@ -22,102 +27,129 @@ impl Controller for LegController {
         let q2 = state.q[2].float();
         let q = vector![q1, q2];
 
-        let q1_dot = state.v[1].float();
-        let q2_dot = state.v[2].float();
-        let q_dot = vector![q1_dot, q2_dot];
+        let v = state.v.to_float_dvec();
+        let dof = v.len();
 
         let q_des = vector![PI / 4., -PI / 2.];
-        let q_ddot_des = (q_des - q) - 0.1 * q_dot;
+        let v_dot_des = (q_des - q) - 0.1 * vector![v[6], v[7]];
+        let v_dot_des = vector![0., 0., 0., 0., 0., 0., v_dot_des[0], v_dot_des[1]];
 
         let l = 0.2;
         let z_com = 1. / 3.
             * (l / 2. * (PI / 4.).cos() + l * (PI / 4.).cos() + l / 2. * (PI / 4. - PI / 2.).cos());
-        flog!("z com: {}", z_com);
+        // flog!("z com: {}", z_com);
         let y_com = 1. / 3. * (-l / 2. * q1.sin() - l * q1.sin() - l / 2. * (q1 + q2).sin());
 
         // Compute J_com systematically
         let J_spatial_vs = state.spatial_velocity_jacobians();
         let mat_linear_v_com = state.com_linear_velocity_extraction_matrices();
 
-        let T_keep_y = Vector3::y().transpose() * &mat_linear_v_com[2]; // T_v_com;
-        let J2_1 = T_keep_y * &J_spatial_vs[1]; // T1_to_world * S1;
-        let J2_2 = T_keep_y * &J_spatial_vs[2]; // T2_to_world * S2;
-        let J2 = vector![J2_1[(0, 0)], J2_2[(0, 0)]];
+        let mut J0: Matrix1xX<Float> = Matrix1xX::zeros(dof);
+        J0.fixed_view_mut::<1, 6>(0, 0)
+            .copy_from(&(Vector3::y().transpose() * &mat_linear_v_com[0] * &J_spatial_vs[0]));
 
-        let J1 = vector![
-            (Vector3::y().transpose() * &mat_linear_v_com[1] * &J_spatial_vs[1])[(0, 0)],
-            0.
-        ];
+        let mut J1: Matrix1xX<Float> = Matrix1xX::zeros(dof);
+        J1.fixed_view_mut::<1, 6>(0, 0)
+            .copy_from(&(Vector3::y().transpose() * &mat_linear_v_com[1] * &J_spatial_vs[0]));
+        J1.fixed_view_mut::<1, 1>(0, 6)
+            .copy_from(&(Vector3::y().transpose() * &mat_linear_v_com[1] * &J_spatial_vs[1]));
+
+        let mut J2 = Matrix1xX::zeros(dof);
+        J2.fixed_view_mut::<1, 6>(0, 0)
+            .copy_from(&(Vector3::y().transpose() * &mat_linear_v_com[2] * &J_spatial_vs[0]));
+        J2.fixed_view_mut::<1, 1>(0, 6)
+            .copy_from(&(Vector3::y().transpose() * &mat_linear_v_com[2] * &J_spatial_vs[1]));
+        J2.fixed_view_mut::<1, 1>(0, 7)
+            .copy_from(&(Vector3::y().transpose() * &mat_linear_v_com[2] * &J_spatial_vs[2]));
 
         // flog!("J1: {}", J1);
         // flog!("J2: {}", J2);
         // flog!("J1 diff: {}", J1 - J1_orig);
         // flog!("J2 diff: {}", J2 - J2_orig);
         // flog!("base transform: {}", bodies_to_root[1].iso);
-        let J = 1. / 3. * (J1 + J2);
+        let J = 1. / 3. * (J0 + J1 + J2);
 
         // Compute J_dot_com systematically
         let J_spatial_v_derivs = state.spatial_velocity_jacobian_derivatives();
         let mat_linear_v_com_derivs = state.com_linear_velocity_extraction_matrix_derivatives();
-        let J1_dot = vector![
-            (Vector3::<Float>::y().transpose()
-                * (&mat_linear_v_com_derivs[1] * &J_spatial_vs[1]
-                    + &mat_linear_v_com[1] * &J_spatial_v_derivs[1]))[(0, 0)],
-            0.
-        ];
-        let J2_dot = vector![
-            (Vector3::<Float>::y().transpose()
-                * (&mat_linear_v_com_derivs[2] * &J_spatial_vs[1]
-                    + &mat_linear_v_com[2] * &J_spatial_v_derivs[1]))[(0, 0)],
-            (Vector3::<Float>::y().transpose()
-                * (&mat_linear_v_com_derivs[2] * &J_spatial_vs[2]
-                    + &mat_linear_v_com[2] * &J_spatial_v_derivs[2]))[(0, 0)],
-        ];
 
-        // flog!("J1 dot diff: {}", J1_dot - J1_dot_orig);
-        // flog!("J2 dot diff: {}", J2_dot - J2_dot_orig);
-        let J_dot = 1. / 3. * (J1_dot + J2_dot);
+        let mut J0_dot: Matrix1xX<Float> = Matrix1xX::zeros(dof);
+        J0_dot.fixed_view_mut::<1, 6>(0, 0).copy_from(
+            &(Vector3::<Float>::y().transpose()
+                * (&mat_linear_v_com_derivs[0] * &J_spatial_vs[0]
+                    + &mat_linear_v_com[0] * &J_spatial_v_derivs[0])),
+        );
+
+        let mut J1_dot = Matrix1xX::zeros(dof);
+        J1_dot.fixed_view_mut::<1, 6>(0, 0).copy_from(
+            &(Vector3::<Float>::y().transpose()
+                * (&mat_linear_v_com_derivs[1] * &J_spatial_vs[0]
+                    + &mat_linear_v_com[1] * &J_spatial_v_derivs[0])),
+        );
+        J1_dot.fixed_view_mut::<1, 1>(0, 6).copy_from(
+            &(Vector3::<Float>::y().transpose()
+                * (&mat_linear_v_com_derivs[1] * &J_spatial_vs[1]
+                    + &mat_linear_v_com[1] * &J_spatial_v_derivs[1])),
+        );
+
+        let mut J2_dot = Matrix1xX::zeros(dof);
+        J2_dot.fixed_view_mut::<1, 6>(0, 0).copy_from(
+            &(Vector3::<Float>::y().transpose()
+                * (&mat_linear_v_com_derivs[2] * &J_spatial_vs[0]
+                    + &mat_linear_v_com[2] * &J_spatial_v_derivs[0])),
+        );
+        J2_dot.fixed_view_mut::<1, 1>(0, 6).copy_from(
+            &(Vector3::<Float>::y().transpose()
+                * (&mat_linear_v_com_derivs[2] * &J_spatial_vs[1]
+                    + &mat_linear_v_com[2] * &J_spatial_v_derivs[1])),
+        );
+        J2_dot.fixed_view_mut::<1, 1>(0, 7).copy_from(
+            &(Vector3::<Float>::y().transpose()
+                * (&mat_linear_v_com_derivs[2] * &J_spatial_vs[2]
+                    + &mat_linear_v_com[2] * &J_spatial_v_derivs[2])),
+        );
+
+        let J_dot = 1. / 3. * (J0_dot + J1_dot + J2_dot);
 
         // Fill in pre-computed Ricatti equation solution S
         let S = DMatrix::from_row_slice(2, 2, &[0.19606829, 0.01922139, 0.01922139, 0.00188435]);
         let B = DMatrix::from_row_slice(2, 1, &[0., 1.]);
-        let y_dot_com = J_dot.tr_mul(&q_dot)[(0, 0)];
+        let y_dot_com = (&J_dot * &v)[(0, 0)];
         let x = vector![y_com, y_dot_com];
 
-        let w_q_ddot = 0.1;
-        let P = ((z_com / GRAVITY).powi(2) * J * &J.transpose()
-            + DMatrix::identity(2, 2).scale(w_q_ddot))
+        let w_v_dot = 0.1;
+        let P = ((z_com / GRAVITY).powi(2) * J.transpose() * &J
+            + DMatrix::identity(dof, dof).scale(w_v_dot))
         .scale(2.);
         let P = CscMatrix::from(P.row_iter());
 
-        let opt_q = ((z_com / GRAVITY).powi(2) * 2. * J_dot.tr_mul(&q_dot)[(0, 0)]
+        let opt_q = ((z_com / GRAVITY).powi(2) * 2. * (&J_dot * &v)[(0, 0)]
             - 2. * z_com / GRAVITY * y_com
             + 2. * x.tr_mul(&(S * B))[(0, 0)])
-            * J
-            - 2. * w_q_ddot * q_ddot_des;
+            * J.transpose()
+            - 2. * w_v_dot * v_dot_des;
 
-        let A = CscMatrix::zeros((0, 2));
-        let b = vec![];
-        let cones = [];
+        let A = Matrix6xX::<Float>::identity(dof);
+        let A = CscMatrix::from(A.row_iter());
+        let b = Vector6::zeros();
+        let cones = [ZeroConeT(6)];
 
         let settings = DefaultSettings::default();
-        let mut solver = DefaultSolver::new(&P, &opt_q.as_slice(), &A, &b, &cones, settings);
+        let mut solver =
+            DefaultSolver::new(&P, &opt_q.as_slice(), &A, &b.as_slice(), &cones, settings);
 
         solver.solve();
 
         let tau = solver.solution.x;
-        flog!(
-            "tau diff: {}",
-            Vector2::from_column_slice(&tau) - q_ddot_des
-        );
+        flog!("tau diff: {}", DVector::from_column_slice(&tau) - v_dot_des);
         flog!("angle diff: {}", q - q_des);
         // let tau = q_ddot_des.clone();
 
         vec![
             JointTorque::Spatial(SpatialVector::zero()),
             // JointTorque::None,
-            JointTorque::Float(tau[0]),
-            JointTorque::Float(tau[1]),
+            JointTorque::Float(tau[6]),
+            JointTorque::Float(tau[7]),
         ]
     }
 }
