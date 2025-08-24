fix: tune zmp weighting & correct z_com
* z_com should be computed from nominal position
* w_v_dot needs to be significantly small for zmp control to show effect
* log y_zmp

python/lqr_leg.py
@@ -6,7 +6,8 @@ B = np.matrix([[0.], [1.]])
 
 C = np.matrix([[1., 0.]])
 
-z_com = 0.09428090415820635
+# z_com = 0.09428090415820635 # up to thigh
+z_com = 0.18970562748477146
 D = -z_com/9.81 * np.identity(1)
 
 Q = C.T * C